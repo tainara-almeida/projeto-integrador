09c40c32fb569b3ed3ac98cd186d34f6f35910e3
AndrewPolengolas <andrewnd2009@outlook.com>
2021-10-18 20:04:46 -0300

    First commit

---
+++ src/main/java/com/pi/entities/Cliente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pi.entities;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author andrew.dornelas
+ */
+public class Cliente implements Serializable{
+    private Long ID;
+    
+    
+}
